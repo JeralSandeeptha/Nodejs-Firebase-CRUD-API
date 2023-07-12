2e21e66971c6bcc076f7d41697ab860d629a8bc2
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2023-07-12 10:17:32 +0530

    Create a error handler

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
+app.all('*', (req, res, next) => {
+    const error = new Error(`Can't find ${req.url} on the server`);
+    error.status = 'fail';
+    error.statusCode = 404;
+    next(error);
+});
+
+app.use( (error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'Error';
+
+    res.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message
+    });
+});
+
 app.listen(6000, () => {
     console.log("Server is running at port 6000");
 });
